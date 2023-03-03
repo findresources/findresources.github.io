7a7d61d59d6f0d6a7578ec1b8bd0ab823bb82bc4
emon <grandth5.00@gmail.com>
2023-03-03 15:30:32 +0600

    added functionality to nav buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+function bringLogin() {
+    let form = document.getElementById('form');
+    let top = document.getElementById('top');
+
+    form.style.display = 'none';
+    top.style.display = 'none';
+}
+
+function bringSignup() {
+    let form = document.getElementById('form');
+    let top = document.getElementById('top');
+
+    form.style.display = 'none';
+    top.style.display = 'none';
+}
+
+function bring2Norm() {
+    let form = document.getElementById('form');
+    let top = document.getElementById('top');
+
+    form.style.display = 'flex';
+    top.style.display = 'flex';
+}
