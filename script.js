d6a6629b78ca31208bda991294821105019d2a51
emon <grandth5.00@gmail.com>
2023-03-04 11:39:45 +0600

    updated the js to bring login functionality

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 function bringLogin() {
     let form = document.getElementById('form');
     let top = document.getElementById('top');
+    let loginDiv = document.getElementById('loginDiv');
+    let navItem1 = document.getElementById('nav-item1');
+    let navItem2 = document.getElementById('loginText');
+    let back = document.getElementById('backBtn');
 
     form.style.display = 'none';
     top.style.display = 'none';
-}
-
-function bringSignup() {
-    let form = document.getElementById('form');
-    let top = document.getElementById('top');
-
-    form.style.display = 'none';
-    top.style.display = 'none';
+    loginDiv.style.display = 'flex';
+    navItem1.style.display = 'none';
+    navItem2.style.display = 'none';
+    back.style.display = 'flex'
 }
 
 function bring2Norm() {
+    let navItem1 = document.getElementById('nav-item1');
+    let navItem2 = document.getElementById('loginText');
     let form = document.getElementById('form');
     let top = document.getElementById('top');
+    let loginDiv = document.getElementById('loginDiv');
+    let back = document.getElementById('backBtn');
 
     form.style.display = 'flex';
     top.style.display = 'flex';
+    loginDiv.style.display = 'none';
+    navItem1.style.display = 'flex';
+    navItem2.style.display = 'flex';
+    back.style.display = 'none';
 }
